a7f7d9fe5152c95a394a11a1e8170857b637e5b0
jacekszmidt <github.jacek@gmail.com>
2019-02-13 23:15:07 +0100

    growing

--- Main.py
+++ Main.py
@@ -15,27 +15,28 @@ class SnakeMove(Enum):
 
 class Snake:
     def __init__(self):
-        self.position = Vector2(200, 200)
+        self.position_list = list()
+        self.position_list.append(Vector2(200, 200))
         self.snake_size = 10
         self.next_move = SnakeMove.UP
         self.snake_color = (0, 0, 0)
 
     def move(self, game_display):
         self.set_next_pos()
-        print(f'current pos {self.position.x}:{self.position.y}')
+        print(f'current pos {self.position_list[0].x}:{self.position_list[0].y}')
         pygame.draw.rect(game_display, self.snake_color,
-                         (self.position.x, self.position.y,
+                         (self.position_list[0].x, self.position_list[0].y,
                           self.snake_size, self.snake_size,))
 
     def set_next_pos(self):
         if self.next_move == SnakeMove.UP:
-            self.position += SnakeMove.UP.value
+            self.position_list[0] += SnakeMove.UP.value
         if self.next_move == SnakeMove.DOWN:
-            self.position += SnakeMove.DOWN.value
+            self.position_list[0] += SnakeMove.DOWN.value
         if self.next_move == SnakeMove.LEFT:
-            self.position += SnakeMove.LEFT.value
+            self.position_list[0] += SnakeMove.LEFT.value
         if self.next_move == SnakeMove.RIGHT:
-            self.position += SnakeMove.RIGHT.value
+            self.position_list[0] += SnakeMove.RIGHT.value
 
     def go_up(self):
         self.next_move = SnakeMove.UP
@@ -94,10 +95,10 @@ class Game:
             self.game_over()
 
     def game_over(self):
-        if self.snake.position.x >= self.display_width\
-                or self.snake.position.y >= self.display_height\
-                or self.snake.position.x >= self.snake.position.x*3.2\
-                or self.snake.position.y >= self.snake.position.y*2.4:
+        if self.snake.position_list[0].x >= self.display_width\
+                or self.snake.position_list[0].y >= self.display_height\
+                or self.snake.position_list[0].x >= self.snake.position_list[0].x*3.2\
+                or self.snake.position_list[0].y >= self.snake.position_list[0].y*2.4:
             self.stop_game = True
             print("Game Over!")
         else:
@@ -116,12 +117,13 @@ class Food:
         self.eaten_food = False
         self.snake = snake
         self.score = 0
+        self.snake.add_snake = False
 
     def printing_food(self, display):
         pygame.draw.rect(display, (0, 0, 255), (self.x_food, self.y_food, 10, 10), 0)
 
     def eating_food(self):
-        if self.x_food == self.snake.position.x and self.y_food == self.snake.position.y:  # checking position
+        if self.x_food == self.snake.position_list[0].x and self.y_food == self.snake.position_list[0].y:  # checking position
                 self.eaten_food = True
                 self.score += 1  # counting points
                 print(self.score)
